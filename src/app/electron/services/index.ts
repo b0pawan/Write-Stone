a349f739965a21c9bffb5d6241909cfdd051a908
b0pawan <asusW3jus>
2017-12-24 21:01:21 +0530

    Write Stone

--- src/app/electron/services/index.ts
+++ src/app/electron/services/index.ts
@@ -1,7 +1,9 @@
 import {RecorderService} from "./recorder.service";
 import {PickerService} from "./picker.service";
+import {ElectronService} from "./electron.service";
 
 export const ELECTRON_SERVICES = [
     RecorderService,
-    PickerService
+    PickerService,
+    ElectronService
 ];
