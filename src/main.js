279fbae799fba4340e19d5326c0ba882d4a34282
b0pawan <asusW3jus>
2017-12-24 14:57:46 +0530

    Write Stone

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+
+let mainWindow;
+let pickerDialog;
+
+app.on('ready', () => {
+  global.ffmpegpath = require('ffmpeg-static').path.replace('app.asar', 'app.asar.unpacked');
+
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+
+  pickerDialog = new BrowserWindow({
+    parent: mainWindow,
+    skipTaskbar: true,
+    modal: true,
+    show: false,
+    height: 390,
+    width: 680
+  });
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  pickerDialog.loadURL('file://' + __dirname + '/picker.html');
+});
+
+ipcMain.on('show-picker', (event, options) => {
+  pickerDialog.show();
+  pickerDialog.webContents.send('get-sources', options);
+});
+
+ipcMain.on('source-id-selected', (event, sourceId) => {
+  pickerDialog.hide();
+  mainWindow.webContents.send('source-id-selected', sourceId);
+});
