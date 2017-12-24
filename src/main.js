8076779d506e37e479e58485a100b893738fb267
b0pawan <asusW3jus>
2017-12-24 16:44:29 +0530

    Write Stone

--- src/main.js
+++ src/main.js
@@ -3,35 +3,44 @@ const {app, BrowserWindow, ipcMain} = require('electron');
 let mainWindow;
 let pickerDialog;
 
+const initializePickerDialog = () => {
+    pickerDialog = new BrowserWindow({
+        parent: mainWindow,
+        skipTaskbar: true,
+        modal: true,
+        show: false,
+        height: 390,
+        width: 680
+    });
+    pickerDialog.loadURL('file://' + __dirname + '/picker.html');
+};
+
 app.on('ready', () => {
-  global.ffmpegpath = require('ffmpeg-static').path.replace('app.asar', 'app.asar.unpacked');
+    global.ffmpegpath = require('ffmpeg-static').path.replace('app.asar', 'app.asar.unpacked');
 
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800
-  });
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    initializePickerDialog();
 
-  pickerDialog = new BrowserWindow({
-    parent: mainWindow,
-    skipTaskbar: true,
-    modal: true,
-    show: false,
-    height: 390,
-    width: 680
-  });
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  pickerDialog.loadURL('file://' + __dirname + '/picker.html');
-  // open dev tools to check console.
-  mainWindow.webContents.openDevTools()
+    // open dev tools to check console.
+    mainWindow.webContents.openDevTools();
+    // picker dialog reinitialize after being closed.
+    pickerDialog.on('closed', (event) => {
+        console.log('picker window closed ', event);
+        initializePickerDialog();
+    });
 });
 
 ipcMain.on('show-picker', (event, options) => {
-  pickerDialog.show();
-  pickerDialog.webContents.send('get-sources', options);
+    pickerDialog.show();
+    pickerDialog.webContents.send('get-sources', options);
 });
 
+
 ipcMain.on('source-id-selected', (event, sourceId) => {
-  pickerDialog.hide();
-  mainWindow.webContents.send('source-id-selected', sourceId);
+    pickerDialog.close();
+    mainWindow.webContents.send('source-id-selected', sourceId);
 });
