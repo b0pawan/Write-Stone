8c7d9ab8057ac75b044156f89ba4276f96f0a136
b0pawan <asusW3jus>
2017-12-24 20:49:19 +0530

    Write Stone

--- src/app/electron/electron.module.ts
+++ src/app/electron/electron.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatSnackBarModule} from "@angular/material";
 import {ELECTRON_SERVICES} from "./services";
+import {PickerComponent} from "./picker.component";
 
 @NgModule({
   imports: [
@@ -9,6 +10,6 @@ import {ELECTRON_SERVICES} from "./services";
     MatSnackBarModule
   ],
   exports: [],
-  declarations: [ELECTRON_SERVICES]
+  declarations: [ELECTRON_SERVICES, PickerComponent]
 })
 export class ElectronAppModule { }
