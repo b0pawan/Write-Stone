7102b14a21ee1db0f6944c7b62759789a2686812
b0pawan <asusW3jus>
2017-12-26 00:33:29 +0530

    Write Stone

--- src/app/electron/electron.module.ts
+++ src/app/electron/electron.module.ts
@@ -3,9 +3,11 @@ import {ELECTRON_SERVICES} from "./services";
 import {NgxElectronModule} from "ngx-electron";
 import {PickerComponent} from "./component/picker.component";
 import {CommonModule} from "@angular/common";
+import {SharedMaterialModule} from "../shared/SharedMaterialModule";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
-  imports: [NgxElectronModule, CommonModule],
+  imports: [NgxElectronModule, SharedModule],
   exports: [PickerComponent, NgxElectronModule],
   providers: [ELECTRON_SERVICES],
   declarations: [PickerComponent]
